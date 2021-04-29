03be86dafc5c40220b72b69698fb275673998d43
mbeckdev <mbeckdev42@gmail.com>
2021-04-28 22:29:14 -0600

    Add initial css and picture

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
+import "./styles.css";
+import Pic from "./kebab-photo.jpeg";
+
 console.log("it's working now!");
 console.log("it's working nw!");
+
+const element = document.createElement("div");
+
+const myPic = new Image();
+myPic.src = Pic;
+element.appendChild(myPic);

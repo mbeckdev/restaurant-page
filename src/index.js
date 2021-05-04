ed47803715e4719957f04ce1ebdce98f5490c811
mbeckdev <mbeckdev42@gmail.com>
2021-05-04 15:45:18 -0600

    Add initial tab coloring

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { loadMenuTab } from "./page-menu.js";
 import { loadAboutTab } from "./page-about.js";
 
 loadPage();
-// loadHomeTab();
-loadMenuTab();
+loadHomeTab();
+// loadMenuTab();
 // loadAboutTab();
 
 const mainTab = document.getElementById("main-tab");
@@ -31,16 +31,26 @@ function clearAndSwitchTab(tab) {
     copyContainer.removeChild(copyContainer.firstChild);
   }
 
+  clearTabColors();
   let theId = tab.id;
   switch (theId) {
     case "main-tab":
+      mainTab.classList.add("tab-selected");
       loadHomeTab();
       break;
     case "menu-tab":
+      menuTab.classList.add("tab-selected");
       loadMenuTab();
       break;
     case "about-tab":
+      aboutTab.classList.add("tab-selected");
       loadAboutTab();
       break;
   }
+}
+
+function clearTabColors() {
+  mainTab.classList.remove("tab-selected");
+  menuTab.classList.remove("tab-selected");
+  aboutTab.classList.remove("tab-selected");
 }
--- src/page-about.js
+++ src/page-about.js
@@ -4,7 +4,15 @@ function loadAboutTab() {
   console.log("about tab loaded");
   const copyContainer = document.getElementById("copy-container");
 
-  // const copyContainer = document.getElementById("copy-container");
+  const p1 = document.createElement("p");
+
+  const p1Text = document.createTextNode(
+    "Forged from the fires of flavor, we've been making kabobs since 1924. We inspect each kabob for maximum tastinesss. It all started when Klaus Kabob was sitting at his forge and became hungry. He wondered, 'What if I could put food on a stick and forge food over a fire?' The rest is history."
+  );
+
+  p1.classList.add("about-p");
+  p1.appendChild(p1Text);
+  copyContainer.appendChild(p1);
 }
 
 export { loadAboutTab };
